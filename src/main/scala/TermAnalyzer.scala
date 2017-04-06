8517794c745af661b0af0181df62f43273d558f2
Mashallah Behbehani <mkbehbehani@gmail.com>
2017-04-05 19:58:52 -0500

    update TermAnalyzer, coalesce results into single textfile output

--- src/main/scala/TermAnalyzer.scala
+++ src/main/scala/TermAnalyzer.scala
@@ -10,9 +10,9 @@ object TermAnalyzer {
   def main(args: Array[String]) {
     val conf = new SparkConf().setAppName("Simple Application").setMaster("local[*]")
     val sc = new SparkContext(conf)
-    val shakespeareFiles: RDD[(String, String)] = sc.wholeTextFiles("shakespeare")
-    val counts = shakespeareFiles.map(_._2).flatMap(_.split("\\W")).map((_, 1)).reduceByKey { case (x, y) => x + y }
-    counts.saveAsTextFile("wordcount4002")
+    val shakespeareFiles: RDD[(String, String)] = sc.wholeTextFiles("/Users/Mashallah/spark-scala-term-analyzer/shakespeare")
+    val counts = shakespeareFiles.map(_._2).flatMap(_.split("\\W")).map((_, 1)).reduceByKey({ case (x, y) => x + y })
+    counts.coalesce(1,true).saveAsTextFile("/Users/Mashallah/spark-scala-term-analyzer/spark-output/2")
     sc.stop()
   }
 }
