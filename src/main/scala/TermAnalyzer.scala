174d327a1ea9b2abf486f0f653ba1bd20835a245
mkbehbehani <mkbehbehani@gmail.com>
2017-04-08 17:54:22 -0500

    update file IO to use relative paths and timestamp naming

--- src/main/scala/TermAnalyzer.scala
+++ src/main/scala/TermAnalyzer.scala
@@ -1,18 +1,15 @@
-/**
-  * Created by Mashallah on 4/5/17.
-  */
-
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.rdd.RDD
+import java.util.Calendar
 
 
 object TermAnalyzer {
   def main(args: Array[String]) {
     val conf = new SparkConf().setAppName("Simple Application").setMaster("local[*]")
     val sc = new SparkContext(conf)
-    val shakespeareFiles: RDD[(String, String)] = sc.wholeTextFiles("/Users/Mashallah/spark-scala-term-analyzer/shakespeare")
+    val shakespeareFiles: RDD[(String, String)] = sc.wholeTextFiles(System.getProperty("user.dir") + "/shakespeare")
     val counts = shakespeareFiles.map(_._2).flatMap(_.split("\\W")).map((_, 1)).reduceByKey({ case (x, y) => x + y })
-    counts.coalesce(1,true).saveAsTextFile("/Users/Mashallah/spark-scala-term-analyzer/spark-output/2")
+    counts.coalesce(1,true).saveAsTextFile(System.getProperty("user.dir") + "/spark-output/" + Calendar.getInstance().getTime.toString)
     sc.stop()
   }
 }
