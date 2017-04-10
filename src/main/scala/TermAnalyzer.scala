f119ae4865521673bf0b2f1c0b2cb8aa0746a26c
mkbehbehani <mkbehbehani@gmail.com>
2017-04-10 00:28:32 -0500

    add commentary

--- src/main/scala/TermAnalyzer.scala
+++ src/main/scala/TermAnalyzer.scala
@@ -4,24 +4,20 @@ import org.apache.spark.SparkContext._
 import java.util.Calendar
 import org.apache.commons.io.FilenameUtils
 
-
-
 object TermAnalyzer {
   def main(args: Array[String]) {
     val conf = new SparkConf().setAppName("Simple Application").setMaster("local[*]")
     val sc = new SparkContext(conf)
     val shakespeareFiles: RDD[(String, String)] = sc.wholeTextFiles(System.getProperty("user.dir") + "/shakespeare")
-    val termAndFiles = shakespeareFiles.map(tup => (FilenameUtils.getBaseName(tup._1), tup._2.split("\\W"))).map(tup => (tup._1, tup._2.map((_,tup._1)))).flatMap(_._2).distinct().groupByKey().map(tup=>(tup._1, tup._2.mkString("[",",", "]")))
-//      val historiesCount =
-//      .map(tup => (tup._1, sc.parallelize(tup._2)).map(tup => (tup._1, tup._2.reduceByKey(_+_)))
-
-      //    val counts2 = sc.makeRDD(counts.)
-//    val supercount =  counts.map(tup => (tup._1, tup._2.PairRDDFunctions.reduceByKey({ case (x: String, y: Int) => x + y })))
-//      .map(tup => (tup._1, tup._2.reduceByKey({ case (x: String, y: Int) => x + y })))
-    //      val termInstances: Array[(String, Int)] = (fileCollection: org.apache.spark.rdd.RDD[(String, String)]) => fileCollection.map(_._2).flatMap(_.split("\\W")).map((_, 1)).reduceByKey({ case (x, y) => x + y })
-    //    val reducedCounts = sc.parallelize(counts.map(tup => (tup._1, tup._2.reduceByKey({ case (x, y) => x + y }))))
-
-    termAndFiles.coalesce(1).saveAsTextFile(System.getProperty("user.dir") + "/spark-output/" + Calendar.getInstance().getTime.toString)
+    val termsAndFiles = shakespeareFiles
+                       .map(tup => (FilenameUtils.getBaseName(tup._1), tup._2.split("\\W"))) // For each file, extract the filename and split the entire textfile into a collection of individual words
+                       .map(tup => (tup._1, tup._2.map((_,tup._1)))) // For every word in the file, create a (word, filename) tuple
+                       .flatMap(_._2) // Discard the filename key, leaving us with (word, filename) tuples
+                       .map(tup => (tup._1.toLowerCase, tup._2)) // Convert all words to lowercase
+                       .distinct()  // Deduplicate file occurrences
+                       .groupByKey() // Group filename occurrences into a tuple of (word, (file1, file2, file3..))
+                       .map(tup=>(tup._1, tup._2.mkString("[",",", "]"))) // Format the file output
+    termsAndFiles.coalesce(1).saveAsTextFile(System.getProperty("user.dir") + "/spark-output/" + Calendar.getInstance().getTime.toString)
     sc.stop()
   }
 }
