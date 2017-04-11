f3b17ec21b07fb5ff75e4da2718e4607d89a7911
mkbehbehani <mkbehbehani@gmail.com>
2017-04-10 23:27:36 -0500

    add length and stopword filters

--- src/main/scala/TermAnalyzer.scala
+++ src/main/scala/TermAnalyzer.scala
@@ -9,7 +9,23 @@ object TermAnalyzer {
     val conf = new SparkConf().setAppName("Simple Application").setMaster("local[*]")
     val sc = new SparkContext(conf)
     val shakespeareFiles: RDD[(String, String)] = sc.wholeTextFiles(System.getProperty("user.dir") + "/hw3data")
-    val termsAndFiles = shakespeareFiles.flatMap(_._2.split("\\r?\\n")).map(s =>(s.split(":::")(1).split("::")(0), s.toLowerCase.split(":::")(2).split("::")(0).split("\\W")))
+    val stopWords = List("about", "above", "after", "again", "against", "all", "am", "an", "and", "any",
+    "are", "arent", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but",
+    "by", "cant", "cannot", "could", "couldnt", "did", "didnt", "do", "does", "doesnt", "doing", "dont", "down",
+    "during", "each", "few", "for", "from", "further", "had", "hadnt", "has", "hasnt", "have", "havent", "having",
+    "he", "hed", "hell", "hes", "her", "here", "heres", "hers", "herself", "him", "himself", "his", "how", "hows",
+    "i", "id", "ill", "im", "ive", "if", "in", "into", "is", "isnt", "it", "its", "its", "itself", "lets", "me",
+    "more", "most", "mustnt", "my", "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other",
+    "ought", "our", "ours ", "ourselves", "out", "over", "own", "same", "shant", "she", "shed", "shell", "shes",
+    "should", "shouldnt", "so", "some", "such", "than", "that", "thats", "the", "their", "theirs", "them",
+    "themselves", "then", "there", "theres", "these", "they", "theyd", "theyll", "theyre", "theyve", "this",
+    "those", "through", "to", "too", "under", "until", "up", "very", "was", "wasnt", "we", "wed", "well", "were",
+    "weve", "were", "werent", "what", "whats", "when", "whens", "where", "wheres", "which", "while", "who", "whos",
+    "whom", "why", "whys", "with", "wont", "would", "wouldnt", "you", "youd", "youll", "youre", "youve", "your",
+    "yours", "yourself", "yourselves")
+
+    val termsAndFiles = shakespeareFiles.flatMap(_._2.split("\\r?\\n"))
+      .map(s =>(s.split(":::")(1).split("::")(0), s.toLowerCase.split(":::")(2).split("::")(0).split("\\W").filter(_.length > 2).filter(!stopWords.contains(_))))
       .map(t => (t._1, t._2.map((t._1, _)))).flatMap(_._2)
       .groupByKey()
       .map(t => (t._1, t._2.map((_,1))))
