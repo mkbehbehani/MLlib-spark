6a88bfa16d3de1208452a202e78f483b689b1b02
mkbehbehani <mkbehbehani@gmail.com>
2017-04-10 23:20:21 -0500

    update code for author and title term extraction

--- src/main/scala/TermAnalyzer.scala
+++ src/main/scala/TermAnalyzer.scala
@@ -8,11 +8,9 @@ object TermAnalyzer {
   def main(args: Array[String]) {
     val conf = new SparkConf().setAppName("Simple Application").setMaster("local[*]")
     val sc = new SparkContext(conf)
-    val shakespeareFiles: RDD[(String, String)] = sc.wholeTextFiles(System.getProperty("user.dir") + "/shakespeare")
-    val termsAndFiles = shakespeareFiles.map(t => (FilenameUtils.getBaseName(t._1), t._2.split("\\W")))
-      .map(t => (t._1, t._2.map((_,t._1))))
-      .flatMap(_._2)
-      .map(t => (t._1.toLowerCase, t._2))
+    val shakespeareFiles: RDD[(String, String)] = sc.wholeTextFiles(System.getProperty("user.dir") + "/hw3data")
+    val termsAndFiles = shakespeareFiles.flatMap(_._2.split("\\r?\\n")).map(s =>(s.split(":::")(1).split("::")(0), s.toLowerCase.split(":::")(2).split("::")(0).split("\\W")))
+      .map(t => (t._1, t._2.map((t._1, _)))).flatMap(_._2)
       .groupByKey()
       .map(t => (t._1, t._2.map((_,1))))
       .map(l=>(l._1, l._2.foldLeft(List[(String, Int)]())((accum, curr)=>{ // array reduction, from this stack overflow: http://stackoverflow.com/questions/30089646/spark-use-reducebykey-on-nested-structure
@@ -22,7 +20,7 @@ object TermAnalyzer {
           case None => curr :: accum
         }
       })))
-      .map(t=>(t._1, t._2.mkString("[",",", "]"))) // Format the file output
+     .map(t=>(t._1, t._2.mkString("[",",", "]"))) // Format the file output
     termsAndFiles.coalesce(1).saveAsTextFile(System.getProperty("user.dir") + "/spark-output/" + Calendar.getInstance().getTime.toString)
     sc.stop()
   }
