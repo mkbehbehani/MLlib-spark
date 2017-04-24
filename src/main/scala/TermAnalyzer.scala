b0a1de5743e1effe5fe80f4452fc9e17bd4e2322
mkbehbehani <mkbehbehani@gmail.com>
2017-04-23 23:03:48 -0500

    restructure  parsing

--- src/main/scala/TermAnalyzer.scala
+++ src/main/scala/TermAnalyzer.scala
@@ -14,40 +14,14 @@ import org.apache.spark.sql.types._
 object TermAnalyzer {
   def main(args: Array[String]) {
     val sparkSession = SparkSession.builder().master("local[*]").getOrCreate()
+    val wineData = sparkSession.sparkContext.textFile("/home/mashallah/IdeaProjects/spark-machine-learning-scala/hwdata/winequality-white.csv")
+    val parsedWineData = wineData.flatMap(_.split("\\n")).map(_.split(";")).map( _.map{y => y.toDouble}).map(y => LabeledPoint(y.last, Vectors.dense(y.slice(0, y.length-1))))
 
-    val customSchema = StructType(Array(
-      StructField("fixed acidity", DoubleType, true),
-      StructField("volatile acidity", DoubleType, true),
-      StructField("citric acid", DoubleType, true),
-      StructField("residual sugar", DoubleType, true),
-      StructField("chlorides", DoubleType, true),
-      StructField("free sulfur dioxide", DoubleType, true),
-      StructField("total sulfur dioxide", DoubleType, true),
-      StructField("density", DoubleType, true),
-      StructField("pH", DoubleType, true),
-      StructField("sulphates", DoubleType, true),
-      StructField("alcohol", DoubleType, true),
-      StructField("quality", DoubleType, true)
-    ))
-
-    val df = sparkSession.read.format("com.databricks.spark.csv").schema(customSchema).option("delimiter", ";").option("header", "true").option("inferSchema", "true").load("/home/mashallah/IdeaProjects/spark-machine-learning-scala/hwdata/winequality-white.csv")
+    val splits = parsedWineData.randomSplit(Array(0.7, 0.3))
+    val (trainingData, testData) = (splits(0), splits(1))
 
 
-    val targetInd = df.columns.indexOf("quality")
-    val ignored = List("quality")
-    val featInd = df.columns.diff(ignored).map(df.columns.indexOf(_))
-    val wineForML = df.rdd.map(r => LabeledPoint(
-      r.getDouble(targetInd), // Get target value
-      // Map feature indices to values
-      Vectors.dense(featInd)
-    ))
-    wineForML.take(4)
-
-    val wineSplit = df.randomSplit(Array(0.7, 0.3))
-    val trainingData = wineSplit(0)
-    val testing = wineSplit(1)
-
-    wineForML.coalesce(1).saveAsTextFile(System.getProperty("user.dir") + "/spark-output/" + Calendar.getInstance().getTime.toString)
+    trainingData.coalesce(1).saveAsTextFile(System.getProperty("user.dir") + "/spark-output/" + Calendar.getInstance().getTime.toString)
 
 
     sparkSession.stop()
