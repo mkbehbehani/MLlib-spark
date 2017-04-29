a99f0f9305d5aa22fc9616fe90c8e74e2bcc2c8e
mkbehbehani <mkbehbehani@gmail.com>
2017-04-29 00:01:00 -0500

    working dataframe label and feature extraction, logistic regression working as well

--- src/main/scala/TermAnalyzer.scala
+++ src/main/scala/TermAnalyzer.scala
@@ -1,7 +1,7 @@
 import java.util.Calendar
 
 import org.apache.spark.ml.classification.{BinaryLogisticRegressionSummary, LogisticRegression}
-import org.apache.spark.ml.feature.LabeledPoint
+import org.apache.spark.ml.feature.{LabeledPoint, VectorAssembler}
 import org.apache.spark.mllib.classification.LogisticRegressionWithLBFGS
 import org.apache.spark.mllib.util.MLUtils
 import org.apache.spark.sql.types.{DoubleType, StructField, StructType}
@@ -33,64 +33,39 @@ object TermAnalyzer {
     ))
 
     val df = spark.read.format("com.databricks.spark.csv").schema(customSchema).option("delimiter", ";").option("header", "true").option("inferSchema", "true").load("/home/mashallah/IdeaProjects/MLlib-spark/hwdata/winequality-white.csv")
-    import spark.implicits._
 
+    val assembler = new VectorAssembler()
+      .setInputCols(Array("fixed acidity",
+        "volatile acidity",
+        "citric acid",
+        "residual sugar",
+        "chlorides",
+        "free sulfur dioxide",
+        "total sulfur dioxide",
+        "density",
+        "pH",
+        "sulphates",
+        "alcohol"))
+      .setOutputCol("features")
+
+    val featurizedData = assembler.transform(df)
+    val output = featurizedData.withColumnRenamed("quality", "label")
+    output.show
 
     val training = spark.read.format("libsvm").load("/home/mashallah/IdeaProjects/MLlib-spark/hwdata/sample_libsvm_data.txt")
 
+
     val lr = new LogisticRegression()
-      .setMaxIter(10)
+      .setMaxIter(100)
       .setRegParam(0.3)
       .setElasticNetParam(0.8)
 
     // Fit the model
-    val lrModel = lr.fit(training)
+    val lrModel = lr.fit(output)
 
-    // Print the coefficients and intercept for logistic regression
-    println(s"Coefficients: ${lrModel.coefficients} Intercept: ${lrModel.intercept}")
-
-    // We can also use the multinomial family for binary classification
-    val mlr = new LogisticRegression()
-      .setMaxIter(10)
-      .setRegParam(0.3)
-      .setElasticNetParam(0.8)
-      .setFamily("multinomial")
-
-    val mlrModel = mlr.fit(training)
-
-    // Print the coefficients and intercepts for logistic regression with multinomial family
-    println(s"Multinomial coefficients: ${mlrModel.coefficientMatrix}")
-    println(s"Multinomial intercepts: ${mlrModel.interceptVector}")
-
-    // Extract the summary from the returned LogisticRegressionModel instance trained in the earlier
-    // example
-    val trainingSummary = lrModel.summary
-
-    // Obtain the objective per iteration.
-    val objectiveHistory = trainingSummary.objectiveHistory
-    println("objectiveHistory:")
-    objectiveHistory.foreach(loss => println(loss))
-
-    // Obtain the metrics useful to judge performance on test data.
-    // We cast the summary to a BinaryLogisticRegressionSummary since the problem is a
-    // binary classification problem.
-    val binarySummary = trainingSummary.asInstanceOf[BinaryLogisticRegressionSummary]
-
-    // Obtain the receiver-operating characteristic as a dataframe and areaUnderROC.
-    val roc = binarySummary.roc
-    roc.show()
-    roc.coalesce(1).write.option("header", "true").csv(System.getProperty("user.dir") + "/spark-output/" + Calendar.getInstance().getTime.toString + "/sample_file.csv")
-    println(s"areaUnderROC: ${binarySummary.areaUnderROC}")
-
-    // Set the model threshold to maximize F-Measure
-    val fMeasure = binarySummary.fMeasureByThreshold
-    val maxFMeasure = fMeasure.select(max("F-Measure")).head().getDouble(0)
-    val bestThreshold = fMeasure.where($"F-Measure" === maxFMeasure)
-      .select("threshold").head().getDouble(0)
-    lrModel.setThreshold(bestThreshold)
-    val resultOutput = s"Coefficients: ${lrModel.coefficients} " +
-                       s"Intercept: ${lrModel.intercept}"
-
+    // Print the coefficients and intercept for multinomial logistic regression
+    println(s"Coefficients: \n${lrModel.coefficientMatrix}")
+    println(s"Intercepts: ${lrModel.interceptVector}")
 
   }
 }
