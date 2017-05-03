94706cde2848a5d23cad92733e2521db8e03d573
mkbehbehani <mkbehbehani@gmail.com>
2017-05-02 19:15:19 -0500

    linear regression from docs

--- src/main/scala/WineAnalyzer.scala
+++ src/main/scala/WineAnalyzer.scala
@@ -53,13 +53,17 @@ object WineAnalyzer {
 
     // Fit the model
     val lrModel = lr.fit(classificationDF)
-//
-//    // Print the coefficients and intercept for multinomial logistic regression
-    println(
-      s"\n${lrModel.coefficients}" +
-      s"\n${lrModel.intercept}" +
-      s"\n${lrModel.summary.totalIterations}" +
-      s"MSE: \n${lrModel.summary.meanSquaredError}")
+
+    // Print the coefficients and intercept for linear regression
+    println(s"Coefficients: ${lrModel.coefficients} Intercept: ${lrModel.intercept}")
+
+    // Summarize the model over the training set and print out some metrics
+    val trainingSummary = lrModel.summary
+    println(s"numIterations: ${trainingSummary.totalIterations}")
+    println(s"objectiveHistory: [${trainingSummary.objectiveHistory.mkString(",")}]")
+    trainingSummary.residuals.show()
+    println(s"RMSE: ${trainingSummary.rootMeanSquaredError}")
+    println(s"r2: ${trainingSummary.r2}")
 
   }
 }
