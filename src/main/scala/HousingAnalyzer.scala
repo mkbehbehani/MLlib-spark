e0e9100de58c28ed37fce37cd7e370f30ed3bd7a
mkbehbehani <mkbehbehani@gmail.com>
2017-06-03 19:38:08 -0500

    add more columns of data, battle string type issue

--- src/main/scala/HousingAnalyzer.scala
+++ src/main/scala/HousingAnalyzer.scala
@@ -1,3 +1,4 @@
+
 import java.util.Calendar
 
 import org.apache.spark.ml.Pipeline
@@ -6,17 +7,99 @@ import org.apache.spark.ml.feature.VectorAssembler
 import org.apache.spark.ml.regression._
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.classification.{BinaryLogisticRegressionSummary, LogisticRegression}
+import org.apache.spark.ml.evaluation.RegressionEvaluator
+import org.apache.spark.ml.feature.{LabeledPoint, VectorAssembler, VectorIndexer}
+import org.apache.spark.ml.regression.{DecisionTreeRegressionModel, DecisionTreeRegressor, LinearRegression, RandomForestRegressor}
+import org.apache.spark.mllib.classification.LogisticRegressionWithLBFGS
+import org.apache.spark.mllib.util.MLUtils
+import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.max
+import org.apache.log4j.{Level, Logger}
 
 object HousingAnalyzer {
 
   def main(args: Array[String]): Unit = {
-
     val spark = SparkSession
       .builder
       .master("local[*]")
       .appName("PipelineExample")
       .getOrCreate()
+    import spark.implicits._
+    val trainingSchema = StructType(Array(
+      StructField("Id", IntegerType, true),
+      StructField("SalePrice", IntegerType, true),
+      StructField("MSSubClass", IntegerType, true),
+      StructField("LotArea", IntegerType, true),
+      StructField("OverallQual", IntegerType, true),
+      StructField("OverallCond", IntegerType, true),
+      StructField("YearBuilt", IntegerType, true),
+      StructField("YearRemodAdd", IntegerType, true),
+      StructField("BsmtFinSF1", IntegerType, true),
+      StructField("BsmtFinSF2", IntegerType, true),
+      StructField("BsmtUnfSF", IntegerType, true),
+      StructField("TotalBsmtSF", IntegerType, true),
+      StructField("1stFlrSF", IntegerType, true),
+      StructField("2ndFlrSF", IntegerType, true),
+      StructField("LowQualFinSF", IntegerType, true),
+      StructField("GrLivArea", IntegerType, true),
+      StructField("BsmtFullBath", IntegerType, true),
+      StructField("BsmtHalfBath", IntegerType, true),
+      StructField("FullBath", IntegerType, true),
+      StructField("HalfBath", IntegerType, true),
+      StructField("BedroomAbvGr", IntegerType, true),
+      StructField("KitchenAbvGr", IntegerType, true),
+      StructField("TotRmsAbvGrd", IntegerType, true),
+      StructField("Fireplaces", IntegerType, true),
+      StructField("GarageCars", IntegerType, true),
+      StructField("GarageArea", IntegerType, true),
+      StructField("WoodDeckSF", IntegerType, true),
+      StructField("OpenPorchSF", IntegerType, true),
+      StructField("EnclosedPorch", IntegerType, true),
+      StructField("3SsnPorch", IntegerType, true),
+      StructField("ScreenPorch", IntegerType, true),
+      StructField("PoolArea", IntegerType, true),
+      StructField("MoSold", IntegerType, true),
+      StructField("YrSold", IntegerType, true)
+    ))
 
+    val testSchema = StructType(Array(
+      StructField("Id", IntegerType, true),
+      StructField("MSSubClass", IntegerType, true),
+      StructField("LotArea", IntegerType, true),
+      StructField("OverallQual", IntegerType, true),
+      StructField("OverallCond", IntegerType, true),
+      StructField("YearBuilt", IntegerType, true),
+      StructField("YearRemodAdd", IntegerType, true),
+      StructField("BsmtFinSF1", IntegerType, true),
+      StructField("BsmtFinSF2", IntegerType, true),
+      StructField("BsmtUnfSF", IntegerType, true),
+      StructField("TotalBsmtSF", IntegerType, true),
+      StructField("1stFlrSF", IntegerType, true),
+      StructField("2ndFlrSF", IntegerType, true),
+      StructField("LowQualFinSF", IntegerType, true),
+      StructField("GrLivArea", IntegerType, true),
+      StructField("BsmtFullBath", IntegerType, true),
+      StructField("BsmtHalfBath", IntegerType, true),
+      StructField("FullBath", IntegerType, true),
+      StructField("HalfBath", IntegerType, true),
+      StructField("BedroomAbvGr", IntegerType, true),
+      StructField("KitchenAbvGr", IntegerType, true),
+      StructField("TotRmsAbvGrd", IntegerType, true),
+      StructField("Fireplaces", IntegerType, true),
+      StructField("GarageCars", IntegerType, true),
+      StructField("GarageArea", IntegerType, true),
+      StructField("WoodDeckSF", IntegerType, true),
+      StructField("OpenPorchSF", IntegerType, true),
+      StructField("EnclosedPorch", IntegerType, true),
+      StructField("3SsnPorch", IntegerType, true),
+      StructField("ScreenPorch", IntegerType, true),
+      StructField("PoolArea", IntegerType, true),
+      StructField("MoSold", IntegerType, true),
+      StructField("YrSold", IntegerType, true)
+    ))
     val rawTrainingData = spark.read.format("com.databricks.spark.csv").option("delimiter", ",").option("header", "true").option("inferSchema", "true").load("/home/mashallah/IdeaProjects/MLlib-spark/source-data/train.csv")
     val rawTestData = spark.read.format("com.databricks.spark.csv").option("delimiter", ",").option("header", "true").option("inferSchema", "true").load("/home/mashallah/IdeaProjects/MLlib-spark/source-data/test.csv")
 
@@ -26,24 +109,29 @@ object HousingAnalyzer {
     ).toMap)
 
     val countFeaturesTestDF = rawTestData.select("Id","MSSubClass","LotArea","OverallQual","OverallCond","YearBuilt","YearRemodAdd","BsmtFinSF1","BsmtFinSF2","BsmtUnfSF","TotalBsmtSF","1stFlrSF","2ndFlrSF","LowQualFinSF","GrLivArea","BsmtFullBath","BsmtHalfBath","FullBath","HalfBath","BedroomAbvGr","KitchenAbvGr","TotRmsAbvGrd","Fireplaces","GarageCars","GarageArea","WoodDeckSF","OpenPorchSF","EnclosedPorch","3SsnPorch","ScreenPorch","PoolArea","MoSold","YrSold")
+    countFeaturesTrainingDF.show(2)
     val nullCorrectedTestDf = countFeaturesTestDF.na.fill(countFeaturesTestDF.columns.zip(
       countFeaturesTestDF.select(countFeaturesTestDF.columns.map(mean): _*).first.toSeq
     ).toMap)
-    val featureColumns = Array("LotArea","1stFlrSF","2ndFlrSF","YearBuilt")
+
+
+    val featureColumns = Array("MSSubClass","LotArea","OverallQual","OverallCond","YearBuilt","YearRemodAdd","BsmtFinSF1","BsmtFinSF2","BsmtUnfSF","TotalBsmtSF","1stFlrSF","2ndFlrSF","LowQualFinSF","GrLivArea","BsmtFullBath","BsmtHalfBath","FullBath","HalfBath","BedroomAbvGr","KitchenAbvGr","TotRmsAbvGrd","Fireplaces","GarageCars","GarageArea","WoodDeckSF","OpenPorchSF","EnclosedPorch","3SsnPorch","ScreenPorch","PoolArea","MoSold","YrSold")
     val labelColumn = "SalePrice"
 
     val assembler = new VectorAssembler()
       .setInputCols(featureColumns)
       .setOutputCol("features")
 
-    val processedTrainingData = assembler.transform(nullCorrectedTrainingDf).select("Id","SalePrice", "features")
-    val processedTestData = assembler.transform(nullCorrectedTestDf).select("Id","features")
-    processedTrainingData.show(3)
-    processedTestData.show(3)
+    val castTrainingData = spark.createDataFrame(nullCorrectedTrainingDf.rdd, trainingSchema)
+    val castTestData = spark.createDataFrame(nullCorrectedTestDf.rdd, testSchema)
 
-    val trainingData = processedTrainingData
-    val testData = processedTestData
-//    val Array(trainingData, testData) = processedTrainingData.randomSplit(Array(0.8, 0.2))
+    val featurizedTrainingData = assembler.transform(castTrainingData).select("Id","SalePrice", "features")
+    val featurizedTestData = assembler.transform(castTestData).select("Id","features")
+
+
+    val trainingData = featurizedTrainingData
+    val testData = featurizedTestData
+//    val Array(trainingData, testData) = featurizedTrainingData.randomSplit(Array(0.8, 0.2))
 
     // Train a DecisionTree model.
     val dt = new DecisionTreeRegressor()
@@ -60,9 +148,9 @@ object HousingAnalyzer {
 
     // Make predictions.
     val predictions2 = model2.transform(testData)
-
+    predictions2.show(3)
     // Select example rows to display.
-    predictions2.withColumnRenamed("prediction", "SalePrice").select("Id","SalePrice").coalesce(1).write.option("header", "true").csv(System.getProperty("user.dir") + "/housing-predictions/" + Calendar.getInstance().getTime.toString)
+//    predictions2.withColumnRenamed("prediction", "SalePrice").select("Id","SalePrice").coalesce(1).write.option("header", "true").csv(System.getProperty("user.dir") + "/housing-predictions/" + Calendar.getInstance().getTime.toString)
     // Select (prediction, true label) and compute test error.
 //    val evaluator2 = new RegressionEvaluator()
 //      .setLabelCol("label")
